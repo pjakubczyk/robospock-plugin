870cba9a57ab6ace036bfc900aa56c88b616b0f9
Przemek Jakubczyk <pjakubczyk@gmail.com>
2014-01-27 15:29:08 +0100

    Cleaning up the specification

--- robospock-plugin/src/test/groovy/org/robospock/RobospockSpecification.groovy
+++ robospock-plugin/src/test/groovy/org/robospock/RobospockSpecification.groovy
@@ -10,78 +10,72 @@ import spock.lang.Specification
  */
 class RobospockSpecification extends Specification {
 
+    static final LIB_PROJECT_NAME = "sample_name"
+    static final SAMPLE_MAVEN_DEP = "com.jakewharton:butterknife:4.0.1"
+
+    static final ANDROID_PLUGIN_PATH = 'com.android.tools.build:gradle:0.7.+'
+
+    Project rootProject
+    Project androidProject
+    RobospockAction robospockAction
+
+    def setup() {
+        rootProject = ProjectBuilder.builder().build()
+        androidProject = createAndroidProject(rootProject)
+        robospockAction = new RobospockAction()
+    }
 
     def "check number of dependencies"() {
+        setup: "create library project"
+        createAndroidLibraryProject(rootProject, LIB_PROJECT_NAME)
 
-        when:
-        Project rootProject = ProjectBuilder.builder().build()
-
-        Project androidProject = createAndroidProject(rootProject)
-
-         createAndroidLibraryProject(rootProject, "aaa")
-
-
+        and: "add dependencies to android project"
         androidProject.dependencies {
-            compile 'com.jakewharton:butterknife:4.0.1'
-            compile rootProject.project("aaa")
+            compile SAMPLE_MAVEN_DEP
+            compile rootProject.project(LIB_PROJECT_NAME)
         }
 
-
-        RobospockAction robospockAction = new RobospockAction()
-
-
+        when: "extract all dependencies"
         def dependencies = robospockAction.getProjectDependencies(androidProject)
 
         then:
         dependencies.size() == 2
     }
 
-    def "check number of maven dependencies"(){
-        when:
-        Project rootProject = ProjectBuilder.builder().build()
+    def "check number of maven dependencies"() {
+        setup: "create library project"
+        createAndroidLibraryProject(rootProject, LIB_PROJECT_NAME)
 
-        Project androidProject = createAndroidProject(rootProject)
-
-        createAndroidLibraryProject(rootProject, "aaa")
-
-
+        and: "add dependencies to android project"
         androidProject.dependencies {
-            compile 'com.jakewharton:butterknife:4.0.1'
-            compile rootProject.project("aaa")
+            compile SAMPLE_MAVEN_DEP
+            compile rootProject.project(LIB_PROJECT_NAME)
         }
 
-
-        RobospockAction robospockAction = new RobospockAction()
-
-
+        and: "extract all dependencies"
         def dependencies = robospockAction.getProjectDependencies(androidProject)
 
-        def maven= robospockAction.getProjectMavenDependencies(dependencies)
+        when: "extract maven dependencies"
+        def maven = robospockAction.getProjectMavenDependencies(dependencies)
 
         then:
         maven.size() == 1
     }
 
-    def "check number of project dependencies"(){
-        when:
-        Project rootProject = ProjectBuilder.builder().build()
+    def "check number of project dependencies"() {
+        setup: "create library project"
+        createAndroidLibraryProject(rootProject, LIB_PROJECT_NAME)
 
-        Project androidProject = createAndroidProject(rootProject)
-
-        createAndroidLibraryProject(rootProject, "aaa")
-
-
+        and: "add dependencies to android project"
         androidProject.dependencies {
-            compile 'com.jakewharton:butterknife:4.0.1'
-            compile rootProject.project("aaa")
+            compile SAMPLE_MAVEN_DEP
+            compile rootProject.project(LIB_PROJECT_NAME)
         }
 
-
-        RobospockAction robospockAction = new RobospockAction()
-
-
+        and: "extract all dependencies"
         def dependencies = robospockAction.getProjectDependencies(androidProject)
 
+        when: "extract library dependencies"
         def libraries = robospockAction.getProjectLibraryDependencies(dependencies)
 
         then:
@@ -99,7 +93,7 @@ class RobospockSpecification extends Specification {
                 mavenCentral()
             }
             dependencies {
-                classpath 'com.android.tools.build:gradle:0.7.+'
+                classpath ANDROID_PLUGIN_PATH
             }
         }
 
@@ -109,29 +103,29 @@ class RobospockSpecification extends Specification {
             mavenCentral()
         }
 
-        return androidProject
+        androidProject
     }
 
     Project createAndroidLibraryProject(Project rootProject, String name) {
-        Project androidProject = ProjectBuilder.builder().withParent(rootProject).withName(name).build()
-        def file = new File(androidProject.rootDir, SdkConstants.FN_LOCAL_PROPERTIES);
+        Project androidLibraryProject = ProjectBuilder.builder().withParent(rootProject).withName(name).build()
+        def file = new File(androidLibraryProject.rootDir, SdkConstants.FN_LOCAL_PROPERTIES);
         file.write("sdk.dir=/home")
 
-        androidProject.buildscript {
+        androidLibraryProject.buildscript {
             repositories {
                 mavenCentral()
             }
             dependencies {
-                classpath 'com.android.tools.build:gradle:0.7.+'
+                classpath ANDROID_PLUGIN_PATH
             }
         }
 
-        androidProject.apply plugin: 'android-library'
+        androidLibraryProject.apply plugin: 'android-library'
 
-        androidProject.repositories {
+        androidLibraryProject.repositories {
             mavenCentral()
         }
 
-        return androidProject
+        androidLibraryProject
     }
 }
