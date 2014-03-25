563b6ad138a1e2a6fdec3128cb2b4075b7108b6b
Przemek Jakubczyk <pjakubczyk@gmail.com>
2014-03-25 13:48:46 +0100

    Bumping version to 0.4:

--- robospock-plugin/src/main/groovy/org/robospock/RobospockPlugin.groovy
+++ robospock-plugin/src/main/groovy/org/robospock/RobospockPlugin.groovy
@@ -1,23 +1,12 @@
 package org.robospock
 
-import org.gradle.api.Action
-import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency
-import org.gradle.api.internal.artifacts.dependencies.DefaultProjectDependency
-import org.gradle.api.plugins.JavaBasePlugin
-import org.gradle.api.tasks.testing.Test
 
 class RobospockPlugin implements Plugin<Project> {
-
-
 
     void apply(Project project) {
         new RobospockAction().execute(project)
     }
-
-
-
 
 }
